b1fa3750ecfc86cbd8a76ee53d2c10d7978d4384
Ana <anarebollo@xpertai.com>
2021-02-22 16:26:45 +0100

    filter portrait image in collage

--- Combinestagram/MainViewController.swift
+++ Combinestagram/MainViewController.swift
@@ -84,6 +84,9 @@ class MainViewController: UIViewController {
     let newPhotos =  photosViewController.selectedPhotos.share()
 
     newPhotos
+      .filter { newImage in
+        newImage.size.width > newImage.size.height
+      }
       .subscribe(onNext: { [weak self] newImage in
         guard let images = self?.images else { return }
         images.value.append(newImage)
