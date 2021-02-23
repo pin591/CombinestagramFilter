ba88768f383181fee62f7c60c4d241ce546b2282
Ana <anarebollo@xpertai.com>
2021-02-23 15:04:06 +0100

    share subscription for load collage and update buttons

--- Combinestagram/MainViewController.swift
+++ Combinestagram/MainViewController.swift
@@ -36,8 +36,12 @@ class MainViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    images.asObservable()
+    
+    let sharedImages =  images.asObservable()
+        .throttle(0.5, scheduler: MainScheduler.instance)
+        .share()
+    
+    sharedImages
       .debounce(0.5, scheduler: MainScheduler.instance)
       .subscribe(onNext: { [weak self] photos in
         guard let preview = self?.imagePreview else { return }
@@ -46,7 +50,7 @@ class MainViewController: UIViewController {
       })
       .disposed(by: bag)
 
-    images.asObservable()
+    sharedImages
       .subscribe(onNext: { [weak self] photos in
         self?.updateUI(photos: photos)
       })
